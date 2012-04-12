db5753a535a42dac4c14b1c6ce346f2f1f6ebfba
Daniel Pinto <danielpinto52@gmail.com>
2012-04-13 00:12:04 +0100

    Added LSR tests

---
+++ tests-6502/unittests/lsr_test.asm
@@ -0,0 +1,56 @@
+.macro getnum character
+  .byte $CF
+.endmacro
+.macro printnum character
+  .byte $DF
+.endmacro
+.macro getchar character
+  .byte $EF
+.endmacro
+.macro printchar character
+  .byte $FF
+.endmacro
+
+.org $1000
+
+; Isto serve para gastar um caracter, porque por
+; algum motivo ele esta la apos o arranque do Qemu...
+getchar
+
+		; Immediate test
+		lda 	#$AA
+loop:	ldy		#0		; Y - carry
+		lsr
+		printnum		; print result
+		tax				; save AC in X
+		bcc		not_c	
+    	iny
+not_c:	lda		#$0A
+		printchar
+		tya
+		printnum		; print carry
+		lda		#$0A
+		printchar
+		txa				; Restore AC
+		bne		loop	; loop until AC is 0
+		
+		; Memory test		
+		ldx 	#$55
+		stx		42		
+loop2:	ldy		#0		; Y - carry
+		lsr		42
+		lda		42
+		printnum		; print result
+		tax				; save AC in X
+		bcc		not_c2	
+    	iny
+not_c2:	lda		#$0A
+		printchar
+		tya
+		printnum		; print carry
+		lda		#$0A
+		printchar
+		txa				; Restore AC
+		bne		loop2	; loop until AC is 0
+end:	jmp 	end		
+
