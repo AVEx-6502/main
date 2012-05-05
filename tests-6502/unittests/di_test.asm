762e3114de60eecc0b92d3d9c050a9522a825eb0
João Jerónimo <joao@torre>
2012-05-05 16:04:06 +0100

    Nex tests for sets and clears of I and D flags...

---
+++ tests-6502/unittests/di_test.asm
@@ -0,0 +1,94 @@
+; Testa o correcto overflow do stack pointer...
+
+
+.macro getnum character
+  .byte $CF
+.endmacro
+.macro printnum character
+  .byte $DF
+.endmacro
+.macro getchar character
+  .byte $EF
+.endmacro
+.macro printchar character
+  .byte $FF
+.endmacro
+.org $1000
+
+; Isto serve para gastar um caracter, porque por
+; algum motivo ele esta la apos o arranque do Qemu...
+getchar                 ;$1000
+    ldx   #$FF          ;$1001
+    txs                 ;$1003
+    jmp   start         ;$1013
+
+; Data area...
+
+
+start:
+
+    cld
+    cli
+    php
+    pla
+    and     #%00011100
+    jsr     printhex        ; 10
+    lda     #' '
+    printchar
+    
+    cld
+    sei
+    php
+    pla
+    and     #%00011100
+    jsr     printhex        ; 14
+    lda     #' '
+    printchar
+    
+    sed
+    sei
+    php
+    pla
+    and     #%00011100
+    jsr     printhex        ; 1C
+    lda     #' '
+    printchar
+    
+    sed
+    cli
+    php
+    pla
+    and     #%00011100
+    jsr     printhex        ; 18
+    lda     #' '
+    printchar
+    
+
+    jmp exit
+
+
+hexchars: .byte "0123456789ABCDEF"
+printhex:
+    tax
+    lsr     A
+    lsr     A
+    lsr     A
+    lsr     A
+    and     #$0F
+    tay
+    lda     hexchars,y
+    printchar
+    
+    txa
+    and     #$0F
+    tay
+    lda     hexchars,y
+    printchar
+    
+    rts
+
+    
+exit:
+lda   #$0A
+printchar
+brk
